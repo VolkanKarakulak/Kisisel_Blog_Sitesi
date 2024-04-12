9619c01ae884b92e30c24e455f43a8da2aaf9248
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-04-12 16:09:17 +0300

    Program.cs, Data ve appsetting ayarları yapıldı.

---
+++ Kisisel_Blog_Sitesi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Kisisel_Blog_Sitesi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kisisel_Blog_Sitesi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<GalleryPhoto> GalleryPhotos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Kisisel_Blog_Sitesi/Program.cs
+++ Kisisel_Blog_Sitesi/Program.cs
@@ -1,3 +1,8 @@
+using Kisisel_Blog_Sitesi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace Kisisel_Blog_Sitesi
 {
 	public class Program
@@ -6,8 +11,18 @@ namespace Kisisel_Blog_Sitesi
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+
+           
+
+            builder.Services.AddControllersWithViews();
 
 			var app = builder.Build();
 
